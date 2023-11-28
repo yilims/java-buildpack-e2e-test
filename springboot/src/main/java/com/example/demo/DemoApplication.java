824a80b1b8085ad9117584699496743265acc7d0
yilims <brightmonkeyli@163.com>
2023-11-28 16:21:01 +0800

    add spring test app

---
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Properties;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/javaversion")
+	public String getJavaVersion() {
+		Properties props = System.getProperties();
+		return props.getProperty("java.version");
+	}
+
+}
