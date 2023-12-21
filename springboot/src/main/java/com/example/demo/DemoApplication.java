997534bbb7f06110d04685eb6197db0c46aba49a
Zhiyong Li <zhiyongli@microsoft.com>
2023-12-21 16:29:44 +0800

    Add JDK Check

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.Properties;
 
@@ -14,10 +15,17 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public static final String SUCCESS = "SUCCESS";
+	public static final String FAILED = "FAILED";
 	@GetMapping("/check")
-	public String getJavaVersion() {
+	public String checkRuntime(@RequestParam(value = "jdk", required = true) String jdkVersion) {
 		Properties props = System.getProperties();
-		return props.getProperty("java.version");
+		if(jdkVersion!=null) {
+			if (!props.getProperty("java.version").startsWith(jdkVersion)) {
+				return FAILED;
+			}
+		}
+		return SUCCESS;
 	}
 
 }
