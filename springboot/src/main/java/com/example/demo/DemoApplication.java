73cbe03863c31948ef76a7bdfba876bf72c903dc
Zhiyong Li <zhiyongli@microsoft.com>
2023-12-22 09:52:52 +0800

    Exception handler

--- springboot/src/main/java/com/example/demo/DemoApplication.java
+++ springboot/src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,5 @@
 package com.example.demo;
 
-import com.example.test.Argument;
-import com.example.test.TestCase;
-import com.example.util.ApplicationContextUtil;
-import java.util.Collection;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ springboot/src/main/java/com/example/demo/ExceptionAdvice.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+@RestControllerAdvice
+public class ExceptionAdvice {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        String msg=sw.toString();
+        return new ResponseEntity<>(msg, null, 500);
+    }
+}
